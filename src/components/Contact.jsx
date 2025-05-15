7bc0044bf4b7842f0c24020b10a6620f643e12f6
Harrison Timothy <arizon.timi@gmail.com>
2025-05-15 09:53:11 +0100

    Update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
       <div className="flex justify-center items-center pt-32">
         <form
           method="POST"
-          action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c"
+          action="https://getform.io/f/anlqvgea"
           className="flex flex-col max-w-[600px] w-full"
         >
           <div className="pb-8  ">
@@ -19,23 +19,26 @@ const Contact = () => {
             </p>
           </div>
           <input
+            required
             className="bg-[#ccd6f6] p-2"
             type="text"
             placeholder="Name"
             name="name"
           />
           <input
+            required
             className="my-4 p-2 bg-[#ccd6f6]"
             type="email"
             placeholder="Email"
             name="email"
           />
           <textarea
+            required
             className="bg-[#ccd6f6] p-2"
             name="message"
             rows="10"
             placeholder="Message"
-          ></textarea>
+          />
           <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
             Let's Collaborate
           </button>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,45 @@
-import React from 'react';
-import { HiArrowNarrowRight } from 'react-icons/hi';
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto px-8 pt-5 flex flex-col justify-center h-full'>
-        <p className='text-pink-600'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
-          HARRISON TIMOTHY
+      <div className="max-w-[1000px] mx-auto px-8 pt-5 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          HARRISON TIMOTHY,
         </h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-          I'm a Front-End Web Developer / Front-End App Developer.
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Frontend Web Developer / Frontend Mobile App Developer.
         </h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a front-end developer specializing in building exceptional digital experiences. Currently, I’m focused on
-          building responsive frontend web and mobile applications.
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I’m a front-end developer specializing in building exceptional digital
+          experiences. Currently, I’m focused on building responsive frontend
+          web and mobile applications.
         </p>
-        <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+        <div className="flex flex-col sm:flex-row gap-4">
+          <Link to="work" smooth={true} duration={500}>
+                   
+          <button className="w-full sm:w-[200px] text-white group border-2 px-6 py-3 flex items-center justify-center hover:bg-pink-600 hover:border-pink-600">
             View Work
-            <span className='group-hover:rotate-90 duration-300'>
-              <HiArrowNarrowRight className='ml-3 ' />
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
             </span>
           </button>
+          </Link>
+          <a
+            href="/HarrisonTimothyCV.pdf"
+            download
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-full sm:w-[200px]"
+          >
+            <button className="w-full text-white group border-2 px-6 py-3 flex items-center justify-center hover:bg-pink-600 hover:border-pink-600">
+              View CV
+            </button>
+          </a>
         </div>
       </div>
     </div>
