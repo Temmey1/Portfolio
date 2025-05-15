df246c2a377410ce6cbcc6234184aff7389cf152
Harrison Timothy <arizon.timi@gmail.com>
2025-05-15 11:45:47 +0100

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
           <Link to="work" smooth={true} duration={500}>
                    
           <button className="w-full sm:w-[200px] text-white group border-2 px-6 py-3 flex items-center justify-center hover:bg-pink-600 hover:border-pink-600">
-            View Work
+            View Works
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3" />
             </span>
